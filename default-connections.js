108893716f748fcf5f7ea57f0a836d405518c6be
Alexandr Ivanov <aivanov@informatica.ru>
2016-12-19 16:26:26 +0300

    add s3

--- default-connections.js
+++ default-connections.js
@@ -9,5 +9,10 @@ module.exports = {
     username: 'docker',
     password: 'docker',
     dbName: 'cameraserver'
+  },
+  s3: {
+    accessKeyId: '4I93XTM0WB8UBX73R7J9',
+    secretAccessKey: 'qwhS5T6AeMKAktqU2wDWL4wGXLPmhU0Gnf5YAysO',
+    endpoint: 'http://10.10.10.242:7480',
   }
 };
